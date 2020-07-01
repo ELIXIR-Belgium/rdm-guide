06021be055dcd0aa1c9954ab8922fc967c46f759
bedroesb <bedro@psb.vib-ugent.be>
2020-07-01 22:00:37 +0200

    toggle fix

--- js/mode-switcher.js
+++ js/mode-switcher.js
@@ -2,19 +2,19 @@ let systemInitiatedDark = window.matchMedia("(prefers-color-scheme: dark)");
 let theme = sessionStorage.getItem('theme');
 
 if (systemInitiatedDark.matches) {
-	document.getElementById("theme-toggle").innerHTML = '<i id="tg-sb-icon" class="fa fa-toggle-off"></i>  Light Mode';
+	document.getElementById("theme-toggle").innerHTML = '<i id="tg-sb-icon" style="color: #de4d00;" class="fa fa-toggle-off"></i>';
 } else {
-	document.getElementById("theme-toggle").innerHTML = '<i id="tg-sb-icon" class="fa fa-toggle-on"></i>  Dark Mode';
+	document.getElementById("theme-toggle").innerHTML = '<i id="tg-sb-icon" style="color: #de4d00;" class="fa fa-toggle-on"></i>';
 }
 
 function prefersColorTest(systemInitiatedDark) {
   if (systemInitiatedDark.matches) {
   	document.documentElement.setAttribute('data-theme', 'dark');		
-   	document.getElementById("theme-toggle").innerHTML = '<i id="tg-sb-icon" class="fa fa-toggle-off"></i>  Light Mode';
+   	document.getElementById("theme-toggle").innerHTML = '<i id="tg-sb-icon" style="color: #de4d00;" class="fa fa-toggle-off"></i>';
    	sessionStorage.setItem('theme', '');
   } else {
   	document.documentElement.setAttribute('data-theme', 'light');
-    document.getElementById("theme-toggle").innerHTML = '<i id="tg-sb-icon" class="fa fa-toggle-on"></i>  Dark Mode';
+    document.getElementById("theme-toggle").innerHTML = '<i id="tg-sb-icon" style="color: #de4d00;" class="fa fa-toggle-on"></i>';
     sessionStorage.setItem('theme', '');
   }
 }
@@ -26,28 +26,28 @@ function modeSwitcher() {
 	if (theme === "dark") {
 		document.documentElement.setAttribute('data-theme', 'light');
 		sessionStorage.setItem('theme', 'light');
-		document.getElementById("theme-toggle").innerHTML = '<i id="tg-sb-icon" class="fa fa-toggle-on"></i>  Dark Mode';
+		document.getElementById("theme-toggle").innerHTML = '<i id="tg-sb-icon" style="color: #de4d00;" class="fa fa-toggle-on"></i>';
 	}	else if (theme === "light") {
 		document.documentElement.setAttribute('data-theme', 'dark');
 		sessionStorage.setItem('theme', 'dark');
-		document.getElementById("theme-toggle").innerHTML = '<i id="tg-sb-icon" class="fa fa-toggle-off"></i>  Light Mode';
+		document.getElementById("theme-toggle").innerHTML = '<i id="tg-sb-icon" style="color: #de4d00;" class="fa fa-toggle-off"></i>';
 	} else if (systemInitiatedDark.matches) {	
 		document.documentElement.setAttribute('data-theme', 'light');
 		sessionStorage.setItem('theme', 'light');
-		document.getElementById("theme-toggle").innerHTML = '<i id="tg-sb-icon" class="fa fa-toggle-on"></i>  Dark Mode';
+		document.getElementById("theme-toggle").innerHTML = '<i id="tg-sb-icon" style="color: #de4d00;" class="fa fa-toggle-on"></i>';
 	} else {
 		document.documentElement.setAttribute('data-theme', 'dark');
 		sessionStorage.setItem('theme', 'dark');
-		document.getElementById("theme-toggle").innerHTML = '<i id="tg-sb-icon" class="fa fa-toggle-off"></i>  Light Mode';
+		document.getElementById("theme-toggle").innerHTML = '<i id="tg-sb-icon" style="color: #de4d00;" class="fa fa-toggle-off"></i>';
 	}
 }
 
 if (theme === "dark") {
 	document.documentElement.setAttribute('data-theme', 'dark');
 	sessionStorage.setItem('theme', 'dark');
-	document.getElementById("theme-toggle").innerHTML = '<i id="tg-sb-icon" class="fa fa-toggle-off"></i>  Light Mode';
+	document.getElementById("theme-toggle").innerHTML = '<i id="tg-sb-icon" style="color: #de4d00;" class="fa fa-toggle-off"></i>';
 } else if (theme === "light") {
 	document.documentElement.setAttribute('data-theme', 'light');
 	sessionStorage.setItem('theme', 'light');
-	document.getElementById("theme-toggle").innerHTML = '<i id="tg-sb-icon" class="fa fa-toggle-on"></i>  Dark Mode';
+	document.getElementById("theme-toggle").innerHTML = '<i id="tg-sb-icon" style="color: #de4d00;" class="fa fa-toggle-on"></i>';
 }
