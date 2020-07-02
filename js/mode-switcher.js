32322ce23b8869b0530176a837f075ac87ed9bbf
bedroesb <bedro@psb.vib-ugent.be>
2020-07-02 11:34:39 +0200

    toggle icon

--- js/mode-switcher.js
+++ js/mode-switcher.js
@@ -2,19 +2,19 @@
 let theme = sessionStorage.getItem('theme');
 
 // if (systemInitiatedDark.matches) {
-// 	document.getElementById("theme-toggle").innerHTML = '<i id="tg-sb-icon" style="color: #de4d00;" class="fa fa-toggle-off"></i>';
+// 	document.getElementById("theme-toggle").innerHTML = '<i style="color: #de4d00;" class="fa fa-adjust"></i>  <i id="tg-sb-icon" style="color: #de4d00;" class="fa fa-toggle-on"></i>';
 // } else {
-// 	document.getElementById("theme-toggle").innerHTML = '<i id="tg-sb-icon" style="color: #de4d00;" class="fa fa-toggle-on"></i>';
+// 	document.getElementById("theme-toggle").innerHTML = '<i style="color: #de4d00;" class="fa fa-adjust"></i>  <i id="tg-sb-icon" style="color: #de4d00;" class="fa fa-toggle-off"></i>';
 // }
 
 // function prefersColorTest(systemInitiatedDark) {
 //   if (systemInitiatedDark.matches) {
 //   	document.documentElement.setAttribute('data-theme', 'dark');		
-//    	document.getElementById("theme-toggle").innerHTML = '<i id="tg-sb-icon" style="color: #de4d00;" class="fa fa-toggle-off"></i>';
+//    	document.getElementById("theme-toggle").innerHTML = '<i style="color: #de4d00;" class="fa fa-adjust"></i>  <i id="tg-sb-icon" style="color: #de4d00;" class="fa fa-toggle-on"></i>';
 //    	sessionStorage.setItem('theme', '');
 //   } else {
 //   	document.documentElement.setAttribute('data-theme', 'light');
-//     document.getElementById("theme-toggle").innerHTML = '<i id="tg-sb-icon" style="color: #de4d00;" class="fa fa-toggle-on"></i>';
+//     document.getElementById("theme-toggle").innerHTML = '<i style="color: #de4d00;" class="fa fa-adjust"></i>  <i id="tg-sb-icon" style="color: #de4d00;" class="fa fa-toggle-off"></i>';
 //     sessionStorage.setItem('theme', '');
 //   }
 // }
@@ -26,28 +26,28 @@ function modeSwitcher() {
 	if (theme === "dark") {
 		document.documentElement.setAttribute('data-theme', 'light');
 		sessionStorage.setItem('theme', 'light');
-		document.getElementById("theme-toggle").innerHTML = '<i id="tg-sb-icon" style="color: #de4d00;" class="fa fa-toggle-on"></i>';
+		document.getElementById("theme-toggle").innerHTML = '<i style="color: #de4d00;" class="fa fa-adjust"></i>  <i id="tg-sb-icon" style="color: #de4d00;" class="fa fa-toggle-off"></i>';
 	}	else if (theme === "light") {
 		document.documentElement.setAttribute('data-theme', 'dark');
 		sessionStorage.setItem('theme', 'dark');
-		document.getElementById("theme-toggle").innerHTML = '<i id="tg-sb-icon" style="color: #de4d00;" class="fa fa-toggle-off"></i>';
+		document.getElementById("theme-toggle").innerHTML = '<i style="color: #de4d00;" class="fa fa-adjust"></i>  <i id="tg-sb-icon" style="color: #de4d00;" class="fa fa-toggle-on"></i>';
 	// } else if (systemInitiatedDark.matches) {	
 	// 	document.documentElement.setAttribute('data-theme', 'light');
 	// 	sessionStorage.setItem('theme', 'light');
-	// 	document.getElementById("theme-toggle").innerHTML = '<i id="tg-sb-icon" style="color: #de4d00;" class="fa fa-toggle-on"></i>';
+	// 	document.getElementById("theme-toggle").innerHTML = '<i style="color: #de4d00;" class="fa fa-adjust"></i>  <i id="tg-sb-icon" style="color: #de4d00;" class="fa fa-toggle-off"></i>';
 	} else {
 		document.documentElement.setAttribute('data-theme', 'dark');
 		sessionStorage.setItem('theme', 'dark');
-		document.getElementById("theme-toggle").innerHTML = '<i id="tg-sb-icon" style="color: #de4d00;" class="fa fa-toggle-off"></i>';
+		document.getElementById("theme-toggle").innerHTML = '<i style="color: #de4d00;" class="fa fa-adjust"></i>  <i id="tg-sb-icon" style="color: #de4d00;" class="fa fa-toggle-on"></i>';
 	}
 }
 
 if (theme === "dark") {
 	document.documentElement.setAttribute('data-theme', 'dark');
 	sessionStorage.setItem('theme', 'dark');
-	document.getElementById("theme-toggle").innerHTML = '<i id="tg-sb-icon" style="color: #de4d00;" class="fa fa-toggle-off"></i>';
+	document.getElementById("theme-toggle").innerHTML = '<i style="color: #de4d00;" class="fa fa-adjust"></i>  <i id="tg-sb-icon" style="color: #de4d00;" class="fa fa-toggle-on"></i>';
 } else if (theme === "light") {
 	document.documentElement.setAttribute('data-theme', 'light');
 	sessionStorage.setItem('theme', 'light');
-	document.getElementById("theme-toggle").innerHTML = '<i id="tg-sb-icon" style="color: #de4d00;" class="fa fa-toggle-on"></i>';
+	document.getElementById("theme-toggle").innerHTML = '<i style="color: #de4d00;" class="fa fa-adjust"></i>  <i id="tg-sb-icon" style="color: #de4d00;" class="fa fa-toggle-off"></i>';
 }
