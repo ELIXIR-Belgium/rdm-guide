63ce678d0f46b1964b476b38552e285ec636bc0b
bedroesb <bedro@psb.vib-ugent.be>
2020-10-23 17:20:13 +0200

    sidebar on top

--- js/customscripts.js
+++ js/customscripts.js
@@ -1,27 +1,5 @@
 $('#mysidebar').height($(".nav").height());
 
-
-$( document ).ready(function() {
-
-    //this script says, if the height of the viewport is greater than 800px, then insert affix class, which makes the nav bar float in a fixed
-    // position as your scroll. if you have a lot of nav items, this height may not work for you.
-    var h = $(window).height();
-    //console.log (h);
-    if (h > 800) {
-        $( "#mysidebar" ).attr("class", "nav affix");
-    }
-    // activate tooltips. although this is a bootstrap js function, it must be activated this way in your theme.
-    $('[data-toggle="tooltip"]').tooltip({
-        placement : 'top'
-    });
-
-    /**
-     * AnchorJS
-     */
-    anchors.add('h2,h3,h4,h5');
-
-});
-
 // needed for nav tabs on pages. See Formatting > Nav tabs for more details.
 // script from http://stackoverflow.com/questions/10523433/how-do-i-keep-the-current-tab-active-with-twitter-bootstrap-after-a-page-reload
 $(function() {
